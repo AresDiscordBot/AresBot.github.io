562ba1affdcb8fa54206eb21df2fcd57e2f51319
AresDiscordBot <122506931+AresDiscordBot@users.noreply.github.com>
2023-03-14 00:11:04 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+function hidev126() {
+  var x = document.getElementById("V1.2.6");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hidev113() {
+  var x = document.getElementById("V1.1.3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hidev107() {
+  var x = document.getElementById("V1.0.7");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hidev104() {
+  var x = document.getElementById("V1.0.4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hidev100() {
+  var x = document.getElementById("V1.0.0");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function startuphide() {
+  document.getElementById("V1.2.6").style.display = "none";
+  document.getElementById("V1.1.3").style.display = "none";
+  document.getElementById("V1.0.7").style.display = "none";
+  document.getElementById("V1.0.4").style.display = "none";
+  document.getElementById("V1.0.0").style.display = "none";
+}
